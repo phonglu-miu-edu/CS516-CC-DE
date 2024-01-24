86b5cc5bf62e6ee5a016cf1baf44268f888e3721
Phong Lu <lutaiphong@gmail.com>
2024-01-23 20:20:00 -0500

    Contact Me button should point to Contact form.

--- portfolio/src/components/ContactButton/index.js
+++ portfolio/src/components/ContactButton/index.js
@@ -4,7 +4,7 @@ const Index = () => {
     return (
         <div className="contact-fixed-button">
             <div className="button-presv">
-                <a href="#0" className="button button-sm bg-white skew" data-scroll-nav="5">
+                <a href="/#contact" className="button button-sm bg-white skew" data-scroll-nav="5">
                     <span className="text-dark">Contact Me</span>
                 </a>
             </div>
